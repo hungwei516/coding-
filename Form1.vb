1f5235c5e54f16a6f6d2ea62f67002b0d3515a6e
hungwei516 <hungwei516@gmail.com>
2026-04-05 13:42:58 +0800

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        For i As Integer = 1 To 4
+            For j As Integer = 1 To i - 1
+                TextBox1.Text = TextBox1.Text & ("0")
+            Next
+            For k As Integer = 1 To 1
+                TextBox1.Text = TextBox1.Text & ("1")
+            Next
+            For l As Integer = 1 To 4 - i
+                TextBox1.Text = TextBox1.Text & ("0")
+            Next
+            TextBox1.Text &= vbCrLf
+        Next
+    End Sub
+End Class
